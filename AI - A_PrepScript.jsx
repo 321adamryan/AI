6bf637d9ffae5b3b4cb6972b9f8e7e98a1d2398b
Adam Ryan <51381387+lazryn@users.noreply.github.com>
2019-08-23 12:14:50 -0400

    Update AI - A_PrepScript.jsx

--- AI - A_PrepScript.jsx
+++ AI - A_PrepScript.jsx
@@ -12,7 +12,7 @@
 
 
 //set document settings
-app.executeMenuCommand('doc-color-cmyk'); 
+app.executeMenuCommand('doc-color-rgb'); 
 
 //Apply to myDoc the active document
 var myDoc = app.activeDocument;
@@ -338,11 +338,66 @@ myLayer.visible = false;
 myLayer.locked = true;
 myLayer.printable = false;
 
-//DRAMATIC PAUSE
+//Delete Unused Swatches
+function deleteUnusedSwatches (doc) {
+	var items = doc.pageItems,
+	len = items.length,
+	sw = doc.swatches,
+	slen = sw.length,
+	arr = [],
+	str = null,
+	i = 0,
+	j,
+	temp = [],
+	p,
+	path,
+	isFill = (function (){
+		p = doc.pathItems.rectangle(0,0,100,100);
+		p.fillColor = sw[2].color;
+		doc.selection = [p];
+		if (sw.getSelected()[0] == sw[2]){
+			p.remove();
+			return 1
+		} else {
+			p.remove();
+			return 0
+		}
+	})();
+
+	for (; i < len; i ++){
+		doc.selection = [items[i]];
+		arr.push(sw.getSelected()[0]);
+		isFill ? (items[i].stroked && temp.push(items[i].strokeColor)) : (items[i].filled && temp.push(items[i].fillColor));
+	}
+
+	while (temp.length){
+		swatch = temp.pop();
+		path = doc.pathItems.rectangle(0,0,100,100);
+		isFill ? (path.fillColor = swatch) : (path.strokeColor = swatch);
+		doc.selection = [path];
+		arr.push(sw.getSelected()[0]);
+		path.remove();
+	}
+
+	str = arr.toString();
+	for (j = slen - 1; j > 1; j --){
+		RegExp(sw[j].name).test(str) || sw[j].remove();
+	}
+}
+deleteUnusedSwatches(app.activeDocument);
+
+
 
 //DELETE LAYER1
- 
+ app.activeDocument.layers.getByName('Layer1').remove();
 
+//DRAMATIC PAUSE
+    wait(6000);  
+    alert('Dramatic..... PAUSE!');  
+      
+    function wait(ms) {  
+         $.sleep(ms); // Milliseconds  
+    }  
 
 
 
